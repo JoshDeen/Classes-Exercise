482572fc09767354a7b062e115d689b1a6558290
Joshua Wilson <joshdeenwilson@gmail.com>
2024-06-11 14:53:33 -0400

    Exercise 1 Completed

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Classes
+﻿using System.Security.AccessControl;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+
+            car1.Year = 2023;
+            car1.Make = "Audi";
+            car1.Model = "R8";
+
+            Console.WriteLine($"I want a { car1.Year} { car1.Make} { car1.Model}");
         }
     }
 }
